e8b38d5b310e3484b188fb3308e82c89935f19f3
srbryers <srbryers@gmail.com>
2024-02-22 09:52:53 -0500

    Return nothing when env variables not set

--- src/utils/contentful.tsx
+++ src/utils/contentful.tsx
@@ -1,22 +1,27 @@
 import { createClient } from 'contentful'
 
-const client = createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: process.env.CONTENTFUL_SPACE_ID || "",
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
-})
 
 function getContent(type: string) {
-  return client
-    .getEntries({
-      content_type: type,
+  if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
+    return []
+  } else {
+    const client = createClient({
+      // This is the space ID. A space is like a project folder in Contentful terms
+      space: process.env.CONTENTFUL_SPACE_ID,
+      // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     })
-    .then((response: any) => response.items)
-    .catch((error: any) => {
-      console.log('Error occurred while fetching Entries')
-      console.error(error)
-    })
+
+    return client
+      .getEntries({
+        content_type: type,
+      })
+      .then((response: any) => response.items)
+      .catch((error: any) => {
+        console.error(error)
+        return []
+      })
+  }
 }
 
 export { getContent }
