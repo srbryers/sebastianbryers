9c883e40bf567b3cb00a6a372b1963d5492585ac
srbryers <srbryers@gmail.com>
2024-02-22 09:37:09 -0500

    Fix build errors

--- src/utils/contentful.tsx
+++ src/utils/contentful.tsx
@@ -1,13 +1,10 @@
 import { createClient } from 'contentful'
 
-if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
-  throw new Error('CONTENTFUL_SPACE_ID and CONTENTFUL_ACCESS_TOKEN environment variables are required')
-}
 const client = createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: process.env.CONTENTFUL_SPACE_ID,
+  space: process.env.CONTENTFUL_SPACE_ID || "",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
 })
 
 function getContent(type: string) {
