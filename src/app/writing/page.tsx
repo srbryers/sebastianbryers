39b4966fcbece0188ee605723fdd7bbb01631851
srbryers <srbryers@gmail.com>
2024-02-22 11:32:07 -0500

    Fix issue with docker build

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -6,9 +6,9 @@ async function getData() {
 
     const client = createClient({
       // This is the space ID. A space is like a project folder in Contentful terms
-      space: process.env.CONTENTFUL_SPACE_ID || "",
+      space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
       // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
     })
   
     return client
