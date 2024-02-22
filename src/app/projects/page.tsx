21b2ce333cc0eb0b13dbd70fc7e50f8e71068e7a
srbryers <srbryers@gmail.com>
2024-02-22 13:41:17 -0500

    Change getData to a server action

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,12 +2,12 @@ import { createClient } from "contentful"
 import { ProjectList } from "@/app/components"
 
 async function getData() {
-
+  'use server'
   const client = createClient({
     // This is the space ID. A space is like a project folder in Contentful terms
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
+    space: process.env.CONTENTFUL_SPACE_ID || process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
   })
 
   return client
